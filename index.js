ee83a7af03557d58e21b151a0979178eb275f0e2
Stefysp <stefaniapantina@gmail.com>
2018-10-31 10:37:41 +0100

    update

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 let client = new jso.JSO({
 	providerID: "HBP",
 	client_id: "9d55e588-19c9-4fce-b72d-3820a0eddee0",
-	redirect_uri: "https://lbologna.github.io/hbp-collab-auth-test/", // The URL where you is redirected back, and where you perform run the callback() function.
+	redirect_uri: "https://script.google.com/macros/s/AKfycbwc67x2o9NS079Ws7n-Dqq1XEf0JbaPmWyPtcGDEaOoxoTghDw/exec", // The URL where you is redirected back, and where you perform run the callback() function.
 	authorization: "https://services.humanbrainproject.eu/oidc/authorize",
 })
 
